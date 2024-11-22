76f1603e8b6304687240bd601976732a9eec43f4
Metin100 <9455@holbertonstudents.com>
2024-11-22 14:07:42 +0400

    idk what i changed:D

--- part3/app/api/v1/auths.py
+++ part3/app/api/v1/auths.py
@@ -23,8 +23,6 @@ class Login(Resource):
 
         if not user or not user.verify_password(credentials['password']):
             return {'error': 'Invalid input'}, 400
-        print(str(user.id))
-        print(str(user.is_admin))
         access_token = create_access_token(identity=str(user.id), additional_claims={"is_admin": user.is_admin})
 
         return {'access_token': access_token}, 200
