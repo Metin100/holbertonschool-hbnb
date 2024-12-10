cc93c2cc12a7632d87d672c00d66d5e63966e539
Metin100 <9455@holbertonstudents.com>
2024-12-10 16:14:41 +0400

    solve place Lists

--- part4/index-script.js
+++ part4/index-script.js
@@ -16,68 +16,71 @@ function getCookie(cname) {
 }
 
 
-function checkAuthentication() {
-    const token = getCookie('token');
+function checkAuthentication(token) {
     const loginLink = document.getElementById('login-link');
-
     if (!token) {
         loginLink.style.display = 'block';
     } else {
         loginLink.style.display = 'none';
-        fetchPlaces(token);
     }
 }
 
-document.addEventListener('DOMContentLoaded', async function fetchPLaces(token){
+document.addEventListener('DOMContentLoaded', async function () {
+    const token = getCookie('token');
+    checkAuthentication(token);
     try {
         const response = await fetch("http://127.0.0.1:6010/api/v1/places", {
             method: "GET",
             headers: {
-                "Authorization": `Bearer ${token}`
+                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
             }
         });
+
         if (response.ok) {
-            data = response.json();
+            const data = await response.json();
             displayPlaces(data);
         } else {
-            console.error('We have problem about response.');
+            console.error(`Response error: ${response.status} - ${response.statusText}`);
         }
+    } catch (error) {
+        console.error(`Fetch error: ${error}`);
     }
-    catch (error) {
-        console.log(`Error: ${error}`);
-    }
-})
+});
+
 
 function displayPlaces(places) {
     const placeList = document.querySelector('#places-list');
+    const priceFilter = document.querySelector('#price-filter');
     placeList.textContent = '';
+    console.log(places)
 
     for (let i = 0; i < places.length; i++) {
         const placeCard = document.createElement('div');
         placeCard.classList.add("place-card");
 
+        const selectPrice = document.createElement('option');
+        selectPrice.value = places[i].price;
+        selectPrice.textContent = `${places[i].price}`
+
         const placeContent = document.createElement('div');
         placeContent.classList.add("place-content");
-        placeCard.appendChild(placeContent);
 
         const placeHeader = document.createElement('div');
         placeHeader.classList.add("place-header");
-        placeHeader.textContent = `${places[i].get('title')}`;
+        placeHeader.textContent = `${places[i].title}`;
 
         const placePrice = document.createElement('div');
         placePrice.classList.add("place-price");
-        placeHeader.textContent = `Price per night: ${places[i].get('price')}`;
+        placePrice.textContent = `Price per night: ${places[i].price}`;
 
         const placeButton = document.createElement('button');
         placeButton.classList.add("place-button");
         placeButton.textContent = 'View Details';
 
+        priceFilter.append(selectPrice)
         placeContent.append(placeHeader, placePrice, placeButton);
+        placeCard.appendChild(placeContent);
         placeList.append(placeCard);
     }
-
-    // document.getElementById('price-filter').addEventListener('change', (event) => {
-    //     // Get the selected price value
-    //     // Iterate over the places and show/hide them based on the selected price
-    // });
 }
